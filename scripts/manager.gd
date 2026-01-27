19f8d98b3671faa8512ef8c5674da585f1096890
Alfonso <alfonsosilvaa29@gmail.com>
2026-01-27 08:57:24 +0100

    Update manager.gd

--- scripts/manager.gd
+++ scripts/manager.gd
@@ -24,21 +24,21 @@ func change_turn():
 
 # Handles the enemy attack logic
 func enemy_attack_logic():
-	# Verificamos que ambos existan y estén vivos
+	# We verified that both exist and are alive.
 	if mob_node and mob_node.esta_vivo and player_node and player_node.vivo:
 		
-		# Elegimos aleatoriamente entre ataque 1 y ataque 2 (50% de probabilidad cada uno)
+		# We randomly choose between attack 1 and attack 2 (50% probability each)
 		var eleccion = randi_range(1, 2)
 		var nombre_ataque = "orc_attack" + str(eleccion)
 		
-		# Ejecutamos la animación y el sonido (usando tu función anterior)
+		# We run the animation and sound (using your previous function)
 		await mob_node.ejecutar_ataque_visual(nombre_ataque)
 		
-		# Lógica de probabilidad de acierto y daño
+		# Logic of probability of success and damage
 		var random_hit = randf() # Genera un número entre 0 y 1
 		
 		if eleccion == 1:
-			# Ataque 1: 90% de probabilidad (0.9) y 20 de daño
+			# Attack 1: 90% chance (0.9) and 20 damage
 			if random_hit <= 0.9:
 				player_node.recibir_danio(20)
 				print("¡Orc Attack 1 acertó! Daño: 20")
@@ -46,12 +46,12 @@ func enemy_attack_logic():
 				print("¡Orc Attack 1 falló!")
 				
 		elif eleccion == 2:
-			# Ataque 2: 40% de probabilidad (0.4) y 80 de daño
+			# Attack 2: 40% chance (0.4) and 80 damage
 			if random_hit <= 0.4:
 				player_node.recibir_danio(80)
 				print("¡CRÍTICO! Orc Attack 2 acertó! Daño: 80")
 			else:
 				print("¡Orc Attack 2 falló!")
 		
-		# Cambiar de turno tras el ataque
+		# Change turns after the attack
 		change_turn()
